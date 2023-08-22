2f05d4720ff4995559ec87ccecdd17728d626fc1
fatimajadoon28 <fatimajadoon28@gmail.com>
2023-08-22 21:06:26 +0500

    your message

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css';
+
+function Footer() {
+    return (
+        <p className={styles.footer}>&copy; H&T Commodities2023</p>
+    )
+}
+
+export default Footer;
